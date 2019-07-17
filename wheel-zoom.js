1c0885fa75470bf2bc15fbbd816580472a4e7518
Worka <bckworka@gmail.com>
2019-07-17 13:39:00 +1000

    Update wheel-zoom.js

--- wheel-zoom.js
+++ wheel-zoom.js
@@ -22,9 +22,9 @@
             this.container = this.image.parentNode;
 
             if (this.image.complete) {
-                this._prepareImage();
+                this._init();
             } else {
-                this.image.onload = this._prepareImage
+                this.image.onload = this._init;
             }
         }
     }
@@ -32,36 +32,38 @@
     JcWheelZoom.prototype = {
         constructor: JcWheelZoom,
         image: null,
-        image_original: {},
         container: null,
-        container_original: {},
+        original: {image: {}, container: {}},
         options: null,
-        _prepareImage: function () {
-            var container_original_rectangle = this.container.getBoundingClientRect();
-
-            this.image_original = {
+        _init: function () {
+            this.original.image = {
                 width: this.image.offsetWidth,
                 height: this.image.offsetHeight
             };
 
-            this.container_original = {
+            this._prepare();
+
+            this.container.addEventListener('mousewheel', this._rescale);
+
+            window.addEventListener('resize', this._rescale);
+        },
+        _prepare: function () {
+            var container_original_rectangle = this.container.getBoundingClientRect();
+
+            this.original.container = {
                 width: this.container.offsetWidth,
                 height: this.container.offsetHeight,
                 left: container_original_rectangle.left,
                 top: container_original_rectangle.top
             };
 
-            if (typeof this.options.prepareImage === 'function') {
-                this.options.prepareImage();
+            if (typeof this.options.prepare === 'function') {
+                this.options.prepare();
             }
 
-            this._rescaleImage();
-
-            window.addEventListener('resize', this._rescaleImage);
-
-            this.container.addEventListener('mousewheel', this._rescaleImage);
+            this._rescale();
         },
-        _rescaleImage: function (event) {
+        _rescale: function (event) {
             var delta = -100000;
 
             var image_current_width = this.image.offsetWidth;
@@ -73,37 +75,32 @@
                 delta = event.wheelDelta > 0 || event.detail < 0 ? 1 : -1;
             }
 
-            var scale = image_current_width / this.image_original.width;
-            var min_scale = Math.min(this.container_original.width / this.image_original.width, this.container_original.height / this.image_original.height);
+            var scale = image_current_width / this.original.image.width;
+            var min_scale = Math.min(this.original.container.width / this.original.image.width, this.original.container.height / this.original.image.height);
             var max_scale = 1;
             var new_scale = scale + (delta / (this.options.speed || 20));
 
             new_scale = (new_scale < min_scale) ? min_scale : (new_scale > max_scale ? max_scale : new_scale);
 
-            // округляем, что бы смягчить скачки значения
-            new_scale = new_scale * 100;
-            new_scale = Math.round(new_scale);
-            new_scale = new_scale / 100;
+            var correct_x = Math.max(0, (this.original.container.width - this.original.image.width * new_scale) / 2);
+            var correct_y = Math.max(0, (this.original.container.height - this.original.image.height * new_scale) / 2);
 
-            var correct_x = Math.round(Math.max(0, (this.container_original.width - this.image_original.width * new_scale) / 2));
-            var correct_y = Math.round(Math.max(0, (this.container_original.height - this.image_original.height * new_scale) / 2));
-
-            this.image.width = this.image_original.width * new_scale;
-            this.image.height = this.image_original.height * new_scale;
+            var image_new_width = this.image.width = this.original.image.width * new_scale;
+            var image_new_height = this.image.height = this.original.image.height * new_scale;
 
             this.image.style.marginLeft = correct_x + 'px';
             this.image.style.marginTop = correct_y + 'px';
 
-            if (typeof this.options.rescaleImage === 'function') {
-                this.options.rescaleImage(new_scale, correct_x, correct_y);
+            if (typeof this.options.rescale === 'function') {
+                this.options.rescale(new_scale, correct_x, correct_y);
             }
 
             if (typeof event !== 'undefined' && event instanceof WheelEvent) {
                 // var isset_left_scroll = !(!(this.container.scrollWidth - this.container.clientWidth));
                 // var isset_top_scroll = !(!(this.container.scrollHeight - this.container.clientHeight));
 
-                var x = Math.round(event.clientX - this.container_original.left + this.container.scrollLeft);
-                var new_x = Math.round(this.image.width * x / image_current_width);
+                var x = Math.round(event.clientX - this.original.container.left + this.container.scrollLeft);
+                var new_x = Math.round(image_new_width * x / image_current_width);
                 var shift_x = new_x - x;
 
                 // if (correct_x > Math.abs(shift_x)) {
@@ -115,8 +112,8 @@
                 this.container.scrollLeft += shift_x;
                 // }
 
-                var y = Math.round(event.clientY - this.container_original.top + this.container.scrollTop);
-                var new_y = Math.round(this.image.height * y / image_current_height);
+                var y = Math.round(event.clientY - this.original.container.top + this.container.scrollTop);
+                var new_y = Math.round(image_new_height * y / image_current_height);
                 var shift_y = new_y - y;
 
                 // if (correct_y > Math.abs(shift_y)) {
