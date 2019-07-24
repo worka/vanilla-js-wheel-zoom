5d7339e72bfbdf1004c23c0bc8e66b84287548e6
Worka <bckworka@gmail.com>
2019-07-24 16:03:57 +1000

    Update wheel-zoom.js

--- wheel-zoom.js
+++ wheel-zoom.js
@@ -19,7 +19,7 @@
         this.options = options;
 
         if (this.image !== null) {
-            this.container = this.image.parentNode;
+            this.window = this.image.parentNode;
 
             if (this.image.complete) {
                 this._init();
@@ -33,98 +33,98 @@
         constructor: JcWheelZoom,
         image: null,
         container: null,
-        original: {image: {}, container: {}},
+        window: null,
+        original: {image: {}, window: {}},
         options: null,
+        correct_x: null,
+        correct_y: null,
         _init: function () {
             this.original.image = {
                 width: this.image.offsetWidth,
                 height: this.image.offsetHeight
             };
 
+            this.container = document.createElement('div');
+
+            this.window.appendChild(this.container);
+            this.container.appendChild(this.image);
+
             this._prepare();
 
-            this.container.addEventListener('mousewheel', this._rescale);
+            this.window.addEventListener('mousewheel', this._rescale);
 
             window.addEventListener('resize', this._rescale);
         },
         _prepare: function () {
-            var container_original_rectangle = this.container.getBoundingClientRect();
+            var window_original_rectangle = this.window.getBoundingClientRect();
 
-            this.original.container = {
-                width: this.container.offsetWidth,
-                height: this.container.offsetHeight,
-                left: container_original_rectangle.left,
-                top: container_original_rectangle.top
+            this.original.window = {
+                width: this.window.offsetWidth,
+                height: this.window.offsetHeight,
+                left: window_original_rectangle.left,
+                top: window_original_rectangle.top
             };
 
-            if (typeof this.options.prepare === 'function') {
-                this.options.prepare();
-            }
+            var min_scale = Math.min(this.original.window.width / this.original.image.width, this.original.window.height / this.original.image.height);
 
-            this._rescale();
+            this.correct_x = Math.max(0, (this.original.window.width - this.original.image.width * min_scale) / 2);
+            this.correct_y = Math.max(0, (this.original.window.height - this.original.image.height * min_scale) / 2);
+
+            this.image.width = this.original.image.width * min_scale;
+            this.image.height = this.original.image.height * min_scale;
+
+            this.image.style.marginLeft = this.correct_x + 'px';
+            this.image.style.marginTop = this.correct_y + 'px';
+
+            this.container.style.width = (this.image.width + (this.correct_x * 2)) + 'px';
+            this.container.style.height = (this.image.height + (this.correct_y * 2)) + 'px';
+
+            if (typeof this.options.prepare === 'function') {
+                this.options.prepare(min_scale, this.correct_x, this.correct_y);
+            }
         },
         _rescale: function (event) {
-            var delta = -100000;
+            event.preventDefault();
 
-            var image_current_width = this.image.offsetWidth;
-            var image_current_height = this.image.offsetHeight;
+            var delta = event.wheelDelta > 0 || event.detail < 0 ? 1 : -1;
 
-            if (typeof event !== 'undefined' && event instanceof WheelEvent) {
-                event.preventDefault();
-
-                delta = event.wheelDelta > 0 || event.detail < 0 ? 1 : -1;
-            }
+            var image_current_width = this.image.width;
+            var image_current_height = this.image.height;
 
             var scale = image_current_width / this.original.image.width;
-            var min_scale = Math.min(this.original.container.width / this.original.image.width, this.original.container.height / this.original.image.height);
+            var min_scale = Math.min(this.original.window.width / this.original.image.width, this.original.window.height / this.original.image.height);
             var max_scale = 1;
-            var new_scale = scale + (delta / (this.options.speed || 20));
+            var new_scale = scale + (delta / 10);
 
             new_scale = (new_scale < min_scale) ? min_scale : (new_scale > max_scale ? max_scale : new_scale);
-
-            var correct_x = Math.max(0, (this.original.container.width - this.original.image.width * new_scale) / 2);
-            var correct_y = Math.max(0, (this.original.container.height - this.original.image.height * new_scale) / 2);
 
             var image_new_width = this.image.width = this.original.image.width * new_scale;
             var image_new_height = this.image.height = this.original.image.height * new_scale;
 
-            this.image.style.marginLeft = correct_x + 'px';
-            this.image.style.marginTop = correct_y + 'px';
+            var container_current_width = image_current_width + (this.correct_x * 2);
+            var container_current_height = image_current_height + (this.correct_y * 2);
+
+            var container_new_width = image_new_width + (this.correct_x * 2);
+            var container_new_height = image_new_height + (this.correct_y * 2);
+
+            this.container.style.width = container_new_width + 'px';
+            this.container.style.height = container_new_height + 'px';
 
             if (typeof this.options.rescale === 'function') {
-                this.options.rescale(new_scale, correct_x, correct_y);
+                this.options.rescale(new_scale, this.correct_x, this.correct_y);
             }
 
-            if (typeof event !== 'undefined' && event instanceof WheelEvent) {
-                // var isset_left_scroll = !(!(this.container.scrollWidth - this.container.clientWidth));
-                // var isset_top_scroll = !(!(this.container.scrollHeight - this.container.clientHeight));
+            var x = Math.round(event.clientX - this.original.window.left + this.window.scrollLeft);
+            var new_x = Math.round(container_new_width * x / container_current_width);
+            var shift_x = new_x - x;
 
-                var x = Math.round(event.clientX - this.original.container.left + this.container.scrollLeft);
-                var new_x = Math.round(image_new_width * x / image_current_width);
-                var shift_x = new_x - x;
+            this.window.scrollLeft += shift_x;
 
-                // if (correct_x > Math.abs(shift_x)) {
-                //     this.image.style.marginLeft = (correct_x + shift_x) + 'px';
-                // } else if (correct_x > 0) {
-                //     this.container.scrollLeft = shift_x + correct_x;
-                //     this.image.style.marginLeft = 0;
-                // } else {
-                this.container.scrollLeft += shift_x;
-                // }
+            var y = Math.round(event.clientY - this.original.window.top + this.window.scrollTop);
+            var new_y = Math.round(container_new_height * y / container_current_height);
+            var shift_y = new_y - y;
 
-                var y = Math.round(event.clientY - this.original.container.top + this.container.scrollTop);
-                var new_y = Math.round(image_new_height * y / image_current_height);
-                var shift_y = new_y - y;
-
-                // if (correct_y > Math.abs(shift_y)) {
-                //     this.image.style.marginTop = (correct_y + shift_y) + 'px';
-                // } else if (correct_y > 0) {
-                //     this.container.scrollTop = shift_y + correct_y;
-                //     this.image.style.marginTop = 0;
-                // } else {
-                this.container.scrollTop += shift_y;
-                // }
-            }
+            this.window.scrollTop += shift_y;
         }
     };
 
