8e2449590934b0c7e1ab46f97d097a037eab676a
worka <bckworka@gmail.com>
2023-09-28 14:21:14 +1000

    added docker

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,11 @@
-docker build --tag 'vanilla-js-wheel-zoom/image' .
-docker run -itd --volume $(pwd)/..:/app --name 'vanilla-js-wheel-zoom' 'vanilla-js-wheel-zoom/image'
-docker exec -it --workdir /app 'vanilla-js-wheel-zoom' sh
+#!/bin/sh
+
+IMAGE_NAME='vanilla-js-wheel-zoom/image'
+CONTAINER_NAME='vanilla-js-wheel-zoom'
+
+LOCAL_DIR="$(pwd)/.."
+WORK_DIR='/app'
+
+docker build --tag $IMAGE_NAME .
+docker run -itd --volume $LOCAL_DIR:$WORK_DIR --name $CONTAINER_NAME $IMAGE_NAME
+docker exec -it --workdir $WORK_DIR $CONTAINER_NAME sh
